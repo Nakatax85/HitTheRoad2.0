edfbb305a7bab4664304fc43eacd0b834d4bafde
Mario Correia <mario_correia_12@hotmail.com>
2017-08-03 14:22:57 +0100

    added database supplies

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -32,3 +32,17 @@ create table user
 )
 ;
 
+INSERT INTO hit_the_road.supplies (name) VALUES ('Canivete Bear Grylls');
+INSERT INTO hit_the_road.supplies (name) VALUES ('Catana');
+INSERT INTO hit_the_road.supplies (name) VALUES ('Desodorizante');
+INSERT INTO hit_the_road.supplies (name) VALUES ('Fruta da Época');
+INSERT INTO hit_the_road.supplies (name) VALUES ('Garrafa de Água');
+INSERT INTO hit_the_road.supplies (name) VALUES ('Lata de Atum');
+INSERT INTO hit_the_road.supplies (name) VALUES ('Lata de Feijões');
+INSERT INTO hit_the_road.supplies (name) VALUES ('Lata de Salsichas');
+INSERT INTO hit_the_road.supplies (name) VALUES ('Papel Higiénico');
+INSERT INTO hit_the_road.supplies (name) VALUES ('Preservativos');
+INSERT INTO hit_the_road.supplies (name) VALUES ('Refresco de Cevada');
+INSERT INTO hit_the_road.supplies (name) VALUES ('Repelente de Insectos');
+INSERT INTO hit_the_road.supplies (name) VALUES ('Saco de Cama');
+INSERT INTO hit_the_road.supplies (name) VALUES ('Toalhetes');
